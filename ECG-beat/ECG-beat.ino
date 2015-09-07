718c0951d0b5fc96d6d4593c4b0085641aec805e
Anne Jan Brouwer <annejan@noprotocol.com>
2015-09-08 00:23:29 +0200

    IJduino heartbeat animation

--- ECG-beat/ECG-beat.ino
+++ ECG-beat/ECG-beat.ino
@@ -3,8 +3,8 @@
 
 ----------------------  Notes ----------------------  ---------------------- 
 This code:
-1) Blinks an LED to User's Live Heartbeat   PIN 13
-2) Fades an LED to User's Live HeartBeat
+1) Blinks a HEART animation
+2) ?? 
 3) Determines BPM
 4) Prints All of the Above to Serial
 
@@ -13,11 +13,27 @@ https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/
  ----------------------       ----------------------  ----------------------
 */
 
+#include "LedControl.h"
+
+/* using VCC, GND, DIN 20, CS 21, CLK 5 for the MAX7219 */
+//static const int DATA_PIN = 12;
+//static const int CS_PIN   = 10;
+//static const int CLK_PIN  = 11;
+static const int DATA_PIN = 20;
+static const int CLK_PIN  = 5;
+static const int CS_PIN   = 21;
+
+LedControl lc=LedControl(DATA_PIN, CLK_PIN, CS_PIN, 1);
+
+
 //  Variables
 int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = 13;                // pin to blink led at each beat
 int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
 int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+int heartFade = 0;
+int heartOpen = 0;
 
 // Volatile Variables, used in the interrupt service routine!
 volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
@@ -38,37 +54,83 @@ void setup(){
    // UN-COMMENT THE NEXT LINE IF YOU ARE POWERING The Pulse Sensor AT LOW VOLTAGE, 
    // AND APPLY THAT VOLTAGE TO THE A-REF PIN
 //   analogReference(EXTERNAL);   
+    /*
+     The MAX72XX is in power-saving mode on startup,
+     we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,1);
+  /* and clear the display */
+  lc.clearDisplay(0);
 }
+
+
+
+const byte heart2[8] PROGMEM = {
+  B00000000,
+  B01100110,
+  B10011001,
+  B10000001,
+  B10000001,
+  B01000010,
+  B00100100,
+  B00011000};
+
+/* here is the data for the characters */
+const byte heart1[8] PROGMEM = {
+  B00000000,
+  B01100110,
+  B11111111,
+  B11111111,
+  B11111111,
+  B01111110,
+  B00111100,
+  B00011000};
+  
 
 
 //  Where the Magic Happens
 void loop(){
   
-    serialOutput() ;       
+    //serialOutput() ;       
     
-  if (QS == true){     //  A Heartbeat Was Found
+  if (QS == true && BPM < 200 && BPM > 50){     //  A Heartbeat Was Found
                        // BPM and IBI have been Determined
                        // Quantified Self "QS" true when arduino finds a heartbeat
         digitalWrite(blinkPin,HIGH);     // Blink LED, we got a beat. 
-        fadeRate = 255;         // Makes the LED Fade Effect Happen
-                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        render(heart1);
+        heartFade = 15;
+        lc.setIntensity(0,heartFade);
+        heartOpen = 100;
+        //delay(200);
+        
         serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.     
         QS = false;                      // reset the Quantified Self flag for next time    
        } 
       else { 
 
-      digitalWrite(blinkPin,LOW);            // There is not beat, turn off pin 13 LED
+      if (heartFade > 0) {
+        heartFade--;
+        lc.setIntensity(0,heartFade);
+      } else if (heartOpen > 0) {
+        heartOpen--;
+      } else if (heartOpen == 0) {
+        heartOpen--;
+        render(heart2);
       }
-     
-   ledFadeToBeat();                      // Makes the LED Fade Effect Happen 
+      digitalWrite(blinkPin,LOW);            // There is not beat, turn off pin 13 LED
+        
+      }
+   
   delay(20);                             //  take a break
 }
 
-
-
-
-
-
-
+int render(const byte* frame) {
+        // render to screen
+        for (int r = 0; r < 8; r++) {
+          lc.setColumn(0, 7-r, pgm_read_byte(&frame[r]));
+        }
+}
 
 
